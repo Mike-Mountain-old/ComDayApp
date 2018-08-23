b4a7cc8ce21991c34144d45f74e91646e0a4d85c
Mike <mike@8327.co.za>
2018-08-23 18:48:55 +0200

    Cleanup

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -14,7 +14,6 @@ const userRoutes: Routes = [
       {
         path: '',
         children: [
-          // {path: 'profile', component: ProfileComponent},
           {path: 'edit-profile', component: ProfileComponent},
           {path: 'user-settings', component: ProfileComponent},
         ]
